2a8684c6aad5a960e91f099859851b8f5a0ab636
Sean Mussack <seanmussack@gmail.com>
2021-03-06 23:41:30 -0600

    Attempted & failed to add PageComponent

---
+++ src/components/PageComponent.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class PageComponent extends Component {
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+    render() {
+        return (
+            <div id="mainContent">
+                
+            </div>
+        );
+    }
+}
+
+export default PageComponent;
